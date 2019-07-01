c95ab17b1ae3747db6eaab2a67c55a9e0354338f
Carlos Mestre <carlos.mestre@gamersclub.com.br>
2019-07-01 16:59:40 -0300

    using promises

--- ajax.js
+++ ajax.js
@@ -12,3 +12,33 @@ xhr.onreadystatechange = function() {
     console.log(JSON.parse(xhr.responseText));
   }
 };
+
+/* #################################################################
+Using XHR
+################################################################# */
+
+var myPromise = function() {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.github.com/users/vserpa");
+    xhr.send(null);
+
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText));
+        } else {
+          reject("Request error");
+        }
+      }
+    };
+  });
+};
+
+var result = myPromise()
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(error) {
+    console.warn(error);
+  });
